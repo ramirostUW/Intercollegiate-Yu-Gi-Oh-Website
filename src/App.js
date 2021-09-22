a827353671c50c753012863b993ee35dec705c4d
Ramiro Steinmann Petrasso <ramirost@live.com>
2021-09-22 02:27:48 -0400

    Starting the repo for the website

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+const App = () => {
+
+  return (
+    <div>
+      <h1>Intercollegiate Yu-Gi-Oh!</h1>
+      <h2> Website coming soon!</h2>
+    </div>
+  );
+};
+
+export default App;
