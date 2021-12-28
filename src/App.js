ffa8d698b052fdcd695d0b7824076b5c24980907
Ramiro Steinmann Petrasso <ramirost@live.com>
2021-12-27 20:08:44 -0800

    Further improvements to overall system stability and other minor adjustments have been made to enhance the user experience.Further improvements to overall system stability and other minor adjustments have been made to enhance the user experience.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
+import './css/index.css';
+import logo from './media/logo.png';
+
 const App = () => {
 
+//&#8209; is a non-line-breaking dash
   return (
     <div>
-      <h1>Intercollegiate Yu-Gi-Oh!</h1>
+      <h1>Intercollegiate Yu&#8209;Gi&#8209;Oh!</h1>
       <h2> Website coming soon!</h2>
+	  <img className={"logoPic"} src={logo} alt="Logo" />
     </div>
   );
+
 };
 
 export default App;
