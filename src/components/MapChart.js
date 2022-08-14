c589773ab55aa8623ab4e48c05dbd43ec5705149
Ramiro Steinmann Petrasso <ramirost@live.com>
2022-08-14 04:22:45 -0400

    Further improvements to overall system stability and other minor adjustments have been made to enhance the user experience.

--- src/components/MapChart.js
+++ src/components/MapChart.js
@@ -1,28 +1,111 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
 import {
   ZoomableGroup,
   ComposableMap,
   Geographies,
-  Geography
+  Geography,
+  Marker
 } from "react-simple-maps";
-import {geoAlbersUsa} from "d3-composite-projections"
+import { Popover, ClickAwayListener } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { geoAlbersUsa } from "d3-composite-projections";
+import ReactTooltip from "react-tooltip";
 
 const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json"
+//const geoUrl = "https://raw.githubusercontent.com/deldersveld/topojson/master/continents/north-america.json";
 
 
-const projection = geoAlbersUsa().translate([600, 240])
+const projection = geoAlbersUsa().translate([400, 240])
+
 
 const MapChart = (props) => {
 
-  //{ setTooltipContent }
+  const [content, setTooltipContent] = useState("");
+  const markers = [
+    { markerOffset: 25, xmarkerOffSet: 0, name: "UW", fullname: "University of Washington", coordinates: [-122.303200, 47.655548], 
+    popoverContent: (
+      <div>
+        <h1>UW Dueling Dawgz</h1>
+        <p>Competed in:</p>
+        <ul>
+          <li>2019 Intercollegiate Championship</li>
+          <li>2021 InterCollegiate Championship</li>
+        </ul>
+        <p>Description:</p>
+        <p>The Dueling Dawgz are the University of Washington's resident Yu-Gi-Oh! Club. 
+          They meet regularly on Fridays at UW's Husky Union Building. Meeting room announcements 
+          can be found on their <a href="https://discord.gg/aMzMMt2nS7">Discord Server</a>.
+        </p>
+      </div>
+    )}, 
+    { markerOffset: 20, xmarkerOffSet: 38, name: "Ohio State", fullname: "Ohio State Unversity", coordinates: [-83.0305, 40.0067], 
+    popoverContent: (<div>Popover Content Placeholder</div>)},
+    { markerOffset: 25, xmarkerOffSet: 0, name: "UNT", fullname: "University of North Texas", coordinates: [-97.1526, 33.2075], 
+    popoverContent: (<div>Popover Content Placeholder</div>) },
+    { markerOffset: -17, xmarkerOffSet: -20, name: "UCR", fullname: "University of California - Riverside", coordinates: [-117.3281, 33.9737], 
+    popoverContent: (<div>Popover Content Placeholder</div>) },
+    { markerOffset: 32, xmarkerOffSet: 40, name: "UCLA", fullname: "University of California - Los Angeles", coordinates: [-118.4452, 34.0689], 
+    popoverContent: (<div>Popover Content Placeholder</div>) },
+    { markerOffset: -15, xmarkerOffSet: 30, name: "UCSD", fullname: "University of California - San Diego", coordinates: [-117.2340, 32.8801], 
+    popoverContent: (<div>Popover Content Placeholder</div>) },
+    { markerOffset: 18, xmarkerOffSet: 15, name: "Arizona State", fullname: "Arizona State University", coordinates: [-111.9281, 33.4242], 
+    popoverContent: (<div>Popover Content Placeholder</div>) },
+    { markerOffset: -10, xmarkerOffSet: 0, name: "UF", fullname: "University of Florida", coordinates: [-82.3549, 29.6436], 
+    popoverContent: (<div>Popover Content Placeholder</div>) },
+    { markerOffset: 20, xmarkerOffSet: -30, name: "MUOH", fullname: "Miami University of Ohio", coordinates: [-84.7345, 39.5087], 
+    popoverContent: (<div>Popover Content Placeholder</div>) },
+    { markerOffset: 0, xmarkerOffSet: 46, name: "Kent State", fullname: "Kent State University", coordinates: [-81.3433, 41.1498], 
+    popoverContent: (<div>Popover Content Placeholder</div>) }
+  ];
+
+
+  const useStyles = makeStyles(theme => ({
+    popoverRoot: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    backDrop: {
+      backdropFilter: "blur(3px)"
+    },
+  }));
+
+
+  let [popoverDisplayed, setPopoverDisplayed] = useState(false);
+  let [popoverContent, setPopoverContent] = useState((<div>Popover Content Placeholder</div>));
+
+  const MoreDetailsPopover = ({ open }) => {
+
+    const classes = useStyles();
+    return (
+        popoverDisplayed && <Popover
+          open
+          anchorReference={"none"}
+          BackdropProps={{
+            onClick: function(){setPopoverDisplayed(false)},
+            classes: {
+              root: classes.backDrop,
+            }
+          }}
+          classes={{
+            root: classes.popoverRoot,
+          }}
+        >
+        {popoverContent}
+        </Popover>
+    )
+  }
+
   return (
     <div id="map">
+      <MoreDetailsPopover />
+      <ReactTooltip>{content}</ReactTooltip>
       <ComposableMap data-tip=""
         projection={projection} >
         <Geographies geography={geoUrl}>
           {({ geographies }) =>
             geographies.map(geo => {
-              
+
               const { NAME, POP_EST } = geo.properties;
               let restingColor = "#E2EDEA";
               let style = {
@@ -33,35 +116,59 @@ const MapChart = (props) => {
                   strokeWidth: "0.4"
                 },
                 hover: {
-                  fill: "#005B67",
-                  outline: "none"
+                  //fill: "#005B67",
+                  fill: restingColor,
+                  outline: "none",
+                  stroke: "#005B67",
+                  strokeWidth: "0.4"
                 },
                 pressed: {
+                  /*
                   fill: "#005B67",
                   outline: "none"
+                  */
+                  fill: restingColor,
+                  outline: "none",
+                  stroke: "#005B67",
+                  strokeWidth: "0.4"
                 }
               }
               return (
                 <Geography
                   key={geo.rsmKey}
                   geography={geo}
-                  onClick={() => {
-                    //const { NAME, POP_EST } = geo.properties;
-                    //setTooltipContent(`${NAME} — ${rounded(POP_EST)}`);
-                  }}
-                  onMouseEnter={() => {
-                    //const { NAME, POP_EST } = geo.properties;
-                    //setTooltipContent(`${NAME} — ${rounded(POP_EST)}`);
-                  }}
-                  onMouseLeave={() => {
-                    //setTooltipContent("");
-                  }}
+                  onClick={() => {}}
+                  onMouseEnter={() => {}}
+                  onMouseLeave={() => {}}
                   style={style}
                 />
               )
             })
           }
         </Geographies>
+        {markers.map(({ name, fullname, coordinates, markerOffset, xmarkerOffSet, popoverContent }) => (
+          <Marker key={name} coordinates={coordinates}
+            onMouseEnter={() => {
+              setTooltipContent(fullname);
+            }}
+            onMouseLeave={() => {
+              setTooltipContent("");
+            }}>
+            <circle r={7.5} fill="#F00" stroke="#fff" strokeWidth={2}
+              onClick={function () {
+                setPopoverContent(popoverContent)
+                setPopoverDisplayed(true)
+              }} />
+            <text
+              textAnchor="middle"
+              x={xmarkerOffSet}
+              y={markerOffset}
+              style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}
+            >
+              {name}
+            </text>
+          </Marker>
+        ))}
       </ComposableMap>
     </div>
   );
