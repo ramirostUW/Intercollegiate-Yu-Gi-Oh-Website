ea72d231f68db4cd82a59647df80855737f2e873
Ramiro Steinmann Petrasso <ramirost@live.com>
2022-09-09 23:37:00 -0400

    pushing react-scripts to production

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {HashRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 import "./bootstrap.min.css";
 import { App } from "./App";
 
